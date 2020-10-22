e4dd04227f9257d770c4991ba4b892599270a034
gavinoconnor <gedmund@gmail.com>
2020-10-22 18:03:11 -0400

    add temp nav layout

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hitting the Bottle</h1>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>NavBar: Episodes | Submissions | Sit With Us | Resources | Shop </h1>
+        <h3>(Sit With Us): Our Story | Contact | Newsletter | Follow Us | Patreon</h3>
+        <h1>Hitting the Bottle</h1>
+        <div>Founder Photo / Mission Statement</div>
+      </div>
+    );
+  }
 }
 
 export default App;
