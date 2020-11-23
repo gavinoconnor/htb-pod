2d08c2273b4b4ae761bfe22dc1ea69d61880edcf
gavinoconnor <gedmund@gmail.com>
2020-11-23 15:44:51 -0500

    add styling to footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
+import './Footer.css';
+
 class Footer extends React.Component {
   render() {
     return (
-      <footer>
+      <footer className='Footer'>
         <ul>
-          <li>2020 Hitting the Bottle</li>
+          <li>© 2020 Hitting the Bottle</li>
           <li>hello@hittingthebottle.com</li>
         </ul>
       </footer>
