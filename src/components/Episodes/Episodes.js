7afb7aceae1a8cbd588c40362ebfc7acb3ff6e2b
gavinoconnor <gedmund@gmail.com>
2020-12-11 16:18:47 -0500

    adjust pod player for mobile

--- src/components/Episodes/Episodes.js
+++ src/components/Episodes/Episodes.js
@@ -6,12 +6,11 @@ class Episodes extends React.Component {
   render() {
     return (
       <div className='episodes'>
-        <h1>Episodes</h1>
         <div className='pod-player'>
-          <iframe title='Latest Episode'
-            style={{ width: '100%', height: '600px' }}
-            frameborder="no"
+          <iframe title='HTB podcast'
+            style={{ width: '100%', height: '600px', borderRadius: '10px', marginBottom: '20px' }}
             scrolling="no"
+            frameborder="no"
             seamless
             src="https://player.captivate.fm/show/00dd4d8f-76c7-4960-850b-2a73acfed083/">
           </iframe>
