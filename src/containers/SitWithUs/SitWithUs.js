3a88daeaed60371437a439eb2c635b0d379fd2ce
gavinoconnor <gedmund@gmail.com>
2020-12-11 16:57:57 -0500

    update Alysia container styles

--- src/containers/SitWithUs/SitWithUs.js
+++ src/containers/SitWithUs/SitWithUs.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import OurStory from '../../components/OurStory/OurStory';
-// import Contact from '../../components/Contact/Contact';
+
 
 import './SitWithUs.css';
 
@@ -9,7 +9,6 @@ class SitWithUs extends React.Component {
   render() {
     return (
       <div className='sitwithus'>
-        <h1>Sit With Us</h1>
         <OurStory />
       </div>
     )
