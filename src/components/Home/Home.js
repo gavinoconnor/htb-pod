7e815b571a25319fa20b6a2c0219a111295b26ee
gavinoconnor <gedmund@gmail.com>
2020-12-17 22:55:47 -0500

    reconfigure background image as part of App

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,20 +3,17 @@ import React from 'react';
 import './Home.css';
 
 // import images
-import backgroundImage from '../../assets/HTB_Final_Girls_1x1.png';
-import founderPhoto from '../../assets/Cheers_final.png';
-import logoImage from '../../assets/HTB_Final_Logo_1x1.png';
+import cheers from '../../assets/Cheers_final.png';
 
 class Home extends React.Component {
 
   render() {
     return (
       <div className='home'>
-      <img src={backgroundImage} alt='' className='bg-image' />
       <div className='topline'>
           <p>two moms. two kids.<br />too many reasons to give two shits.</p> 
         </div>
-        <img src={founderPhoto} alt='' className='cheers-photo' />
+        <img src={cheers} alt='' className='cheers-photo' />
         <div className='bottomline'>
           <p>shake your beverages. not your babies.</p>
         </div>
