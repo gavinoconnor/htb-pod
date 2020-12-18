8b3ee61fb437a42b57c1182b9f3f89612769b482
gavinoconnor <gedmund@gmail.com>
2020-12-18 17:00:39 -0500

    add copy for patreon

--- src/components/Patreon/Patreon.js
+++ src/components/Patreon/Patreon.js
@@ -2,12 +2,19 @@ import React from 'react';
 
 import './Patreon.css';
 
+import patreonMark from '../../assets/Digital-Patreon-Wordmark_WhiteOnBlack-Sm.jpg';
+
 class Patreon extends React.Component {
   render() {
     return (
       <div className='patreon'>
-        <h1>Patreon info coming soon!</h1>
-
+        <p>
+          Signing up for our Patreon gives you early access to episodes, bonus content, special discounts, private community social groups, and more.  Beyond that - it is a huge support that allows us to grow and bring you more amazing content each week! It’s a win-WIN.  Choose your tier (ranging from $5 - $25 / month) and start getting that special treatment today!
+        </p>
+        <div className='mark'>
+          <a href='https://www.patreon.com/hittingthebottle'>
+          <img src={patreonMark} alt='' className='patreon-mark' /></a>
+        </div>
       </div>
     )
   }
--- src/components/SpillTheWhine/SpillTheWhine.js
+++ src/components/SpillTheWhine/SpillTheWhine.js
@@ -6,21 +6,29 @@ class SpillTheWhine extends React.Component {
   render() {
     return (
       <div className='spill'>
-        <p>
-          Is it your time to Spill the Whine? Got a juicy story you’ve never shared with anyone? No time like the present. All the stories you’ve heard on our podcast have been submitted by listeners LIKE YOU.  We want to hear it all. A harrowing birth story? A comically ill-timed blowout? Debilitating postpartum anxiety? From small to the extremely big - we are here to be vulnerable and share the things that no one talks about. We are creating space for the good, the bad, and the exceptionally hideous. Join us.
+        <section>
+          <p>
+            Is it your time to Spill the Whine? Got a juicy story you’ve never shared with anyone? No time like the present. All the stories you’ve heard on our podcast have been submitted by listeners LIKE YOU.  We want to hear it all. A harrowing birth story? A comically ill-timed blowout? Debilitating postpartum anxiety? From small to the extremely big - we are here to be vulnerable and share the things that no one talks about. We are creating space for the good, the bad, and the exceptionally hideous. Join us.
         </p>
 
-        <p>
-          You can record yourself telling a story right from your phone! Then email us the audio file and any corresponding photos you would like to be featured on our <a href='https://www.instagram.com/hittingthebottle/' target='_blank' rel="noopener noreferrer">@<u>hittingthebottle</u></a> Instagram page.
+          <p>
+            You can record yourself telling a story right from your phone! Then email us the audio file and any corresponding photos you would like to be featured on our <a href='https://www.instagram.com/hittingthebottle/' target='_blank' rel="noopener noreferrer">@<u>hittingthebottle</u></a> Instagram page.
         </p>
 
-        <p>
-          It must be said - we appreciate you all very much.  Creating this community and providing space for your stories is a dream for us.  We cannot wait to see what we can build and hope you’ll stick by our side through all the tears, laughter, and inevitable cocktail-fueled sidebars.
+          <p>
+            It must be said - we appreciate you all very much.  Creating this community and providing space for your stories is a dream for us.  We cannot wait to see what we can build and hope you’ll stick by our side through all the tears, laughter, and inevitable cocktail-fueled sidebars.
         </p>
 
-        <p>
-          Submit all audio files and corresponding photographs to us at <a href='mailto:hello@hittingthebottlepod.com'><u>hello@hittingthebottlepod.com</u>.</a>
+          <p>
+            Submit all audio files and corresponding photographs to us at <a href='mailto:hello@hittingthebottlepod.com'><u>hello@hittingthebottlepod.com</u>.</a>
+          </p>
+        </section>
+        {/* <section>
+          <p>
+            Can’t hit the bottle hard enough? Subscribe to our Newsletter here! Receive special offers, episode reminders, upcoming events, and so much more. We won’t send more than one a week - if that. Promise! We don’t have the time!
         </p>
+        
+        </section> */}
       </div>
     )
   }
