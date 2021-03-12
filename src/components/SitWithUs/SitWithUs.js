48c0b4fb815c4bae97668af292c337fa6216c057
gavinoconnor <gedmund@gmail.com>
2021-03-12 17:40:38 -0500

    reassign grid elements

--- src/components/SitWithUs/SitWithUs.js
+++ src/components/SitWithUs/SitWithUs.js
@@ -14,7 +14,7 @@ class OurStory extends React.Component {
         <div className="alysia">
           <img src={Alysia} alt="alysia" style={{ borderRadius: '15%' }} />
 
-          <div className="sit-social-icons">
+          <div className="sit-social-alysia">
             <a href='httsections://www.instagram.com/hittingthebottle/' target='_blank' rel="noopener noreferrer"><i className='fab fa-instagram fa-lg fa-fw'></i>
             </a>
             <a href='https://www.facebook.com/hittingthebottlepod/' target='_blank' rel="noopener noreferrer"><i className='fab fa-facebook fa-lg fa-fw'></i>
@@ -23,11 +23,11 @@ class OurStory extends React.Component {
             </a>
           </div>
 
-          <div className='sit-bio'>
+          <div className='sit-bio-alysia'>
             <h3 className="sit-name">Alysia</h3>
             <div className='sit-about-alysia'>
               <p>
-                Originally hails from Los Angeles, has now resided in the Boston area for more than half a decade. Ask her four years ago if she pictured herself with a kid and she would’ve poured her shot of tequila on you. However, here she (happily!) is. She is a novice mom to 3-year-old River James, who is as sparkling, fierce, and untamed as her name might suggest.
+                Originally hails from Los Angeles and has now resided in the Boston area for more than half a decade. Ask her four years ago if she pictured herself with a kid and she would’ve poured her shot of tequila on you. However, here she (happily!) is. She is a novice mom to 3-year-old River James, who is as sparkling, fierce, and untamed as her name might suggest.
               </p>
               <p>
                 When not fighting the good fight we lovingly call parenting, Alysia spends her time dabbling in interior design, reading the stack of books she keeps bedside, and playing chef in the kitchen. Alongside Sarah, she’s ready to have inclusive conversations with parents about the good shit, the bad shit and the in-between.
@@ -45,7 +45,7 @@ class OurStory extends React.Component {
         <div className="sarah">
           <img src={Sarah} alt="sarah" style={{ borderRadius: '15%' }} />
 
-          <div className="social-icons">
+          <div className="sit-social-sarah">
             <a href='httsections://www.instagram.com/hittingthebottle/' target='_blank' rel="noopener noreferrer"><i className='fab fa-instagram fa-lg fa-fw'></i>
             </a>
             <a href='https://www.facebook.com/hittingthebottlepod/' target='_blank' rel="noopener noreferrer"><i className='fab fa-facebook fa-lg fa-fw'></i>
@@ -54,17 +54,20 @@ class OurStory extends React.Component {
             </a>
           </div>
 
-          <div className="sit-bio">
+          <div className="sit-bio-sarah">
             <h3 className="sit-name">Sarah</h3>
             <div className='sit-about-sarah'>
               <p>
-                Is a self-proclaimed mom expert who truly has no idea what the f*ck she is doing. Fake it ‘til you make it, isn’t that what they say?  Coming from a diverse background including - but not limited to - corporate retail, design, and a quick-but-very-fun stint in bartending, motherhood was a far cry from her career path. Not one to back down, she accepted the role and is doing the best she can. She is a single mama to a sprightly three-year-old Owen Fox, who is an actual tornado of love and mischief.
+                Is a self-proclaimed mom expert who truly has no idea what the f*ck she is doing. Fake it ‘til you make it, isn’t that what they say?  Coming from a diverse background including - but not limited to - corporate retail, design, and a quick-but-very-fun stint in bartending, motherhood was a far cry from her career path. Not one to back down, she accepted the role and is doing the best she can.
               </p>
               <p>
-                After quite the life-changing move from the east coast to the west to help a friend launch a new business...a global pandemic hit.  So what does a single mother, unemployed, living across the country from her support system do? Her best to create a community of people who also have no fucking clue what they are doing, drink in hand.
+                She is a single mama to a sprightly three-year-old Owen Fox, who is an actual tornado of love and mischief. After quite the life-changing move from the east coast to the west to help a friend launch a new business...a global pandemic hit.
               </p>
               <p>
-                Alongside Alysia, Sarah wants to create a safe space to talk about shit: the good, the bad, and the especially hideous. Time to slide down a bourbon on the rocks, sit crooked, and talk straight.
+                So what does a single mother, unemployed, living across the country from her support system do? Her best to create a community of people who also have no fucking clue what they are doing, drink in hand. Sarah wants to create a safe space to talk about shit: the good, the bad, and the especially hideous.
+              </p>
+              <p>
+                Time to slide down a bourbon on the rocks, sit crooked, and talk straight.
               </p>
             </div>
           </div>
